b9660765c84e9e1c799d94147dc627efac01a8d1
Manuel Spínola <mspinola10@gmail.com>
2026-06-05 09:13:45 -0600

    fix app.R header for RStudio Run App button

--- app.R
+++ app.R
@@ -1,3 +1,6 @@
+# Launch the ShinyApp (Do not remove this comment)
+# To deploy, run: rsconnect::deployApp()
+# Or use the blue button on top of this file
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 
 source("/Users/manuel_nuevo/R_MS/R_packages/h3sdm/R/h3sdm_aoa.R")
@@ -14,3 +17,4 @@ assignInNamespace("h3sdm_pa_from_records", h3sdm_pa_from_records, ns = "h3sdm")
 
 options("golem.app.prod" = TRUE)
 StatH3sdm::run_app()
+
